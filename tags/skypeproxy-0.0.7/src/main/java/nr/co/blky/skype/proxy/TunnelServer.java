cf966b7478b645fa495b0a162070ad76b5f5482c
vasiliij.pupkin <vasiliij.pupkin@128fde70-b5a7-11dd-8d59-c9fed4921599>
2009-03-25 00:24:17 +0000

    [maven-release-plugin]  copy for tag skypeproxy-0.0.7

---
+++ tags/skypeproxy-0.0.7/src/main/java/nr/co/blky/skype/proxy/TunnelServer.java
@@ -0,0 +1,47 @@
+package nr.co.blky.skype.proxy; 
+import java.io.PrintStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory; 
+import com.skype.ApplicationListener;
+import com.skype.SkypeException;
+import com.skype.Stream;
+
+public class TunnelServer implements ApplicationListener {
+
+	static final String HOST = "host";
+	static final String LISTENHOST = "listenhost";
+	static final String PORT = "port";
+	static final String LISTENPORT	= "listenport";
+	public static final String EOL = "\n";
+	public static final String ID = "SiD";
+	
+	static Log log = LogFactory.getLog(TunnelServer.class);
+  	private static int id = 0;
+	PrintStream out = System.out;
+  	SkypeRelay  sr =null;
+	
+	public TunnelServer(PrintStream out) {
+		this.out = out;
+ 	}
+
+
+	/**
+	 * accept all connections
+	 */
+	public void connected(Stream stream) throws SkypeException{ 
+	      // connect to the thing I'm tunnelling for 
+			log.debug("connected: A::"+stream.getApplication()+" SiD::"+stream.getId()+"{"+stream+"}");
+			sr = new SkypeRelay ( 	stream,  	this.out  	); 
+			stream.addStreamListener(sr);
+			new Thread(sr,"@skypeproxy#"+id ++).start(); 
+	    }
+
+	public void disconnected(Stream arg0) throws SkypeException {
+		sr.destroy();
+	}
+
+}
+
+
+ 
