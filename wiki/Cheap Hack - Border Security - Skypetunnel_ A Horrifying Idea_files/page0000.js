9e355a00c99971339666afd56cdaf1cbd33daf5a
vasiliij.pupkin@gmail.com <vasiliij.pupkin@gmail.com@128fde70-b5a7-11dd-8d59-c9fed4921599>
2011-04-06 21:26:04 +0000

    Autoversioning commit:  a non-deltaV client made a change to /wiki/Cheap Hack - Border Security - Skypetunnel_ A Horrifying Idea_files/page0000.js

--- wiki/Cheap Hack - Border Security - Skypetunnel_ A Horrifying Idea_files/page0000.js
+++ wiki/Cheap Hack - Border Security - Skypetunnel_ A Horrifying Idea_files/page0000.js
@@ -1,1 +1,22 @@
-1
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 20:53:27 Jul 2, 2007 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 20:34:47 Apr 6, 2011.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+function resizeIframe(id){
+	var newheight;
+	if (navigator.appName != "Microsoft Internet Explorer" && !window.opera && !document.mimeType && document.all && document.getElementById){
+		newheight=document.getElementById(id).contentWindow.document.body.offsetHeight;
+	}else if(document.getElementById){
+		newheight=document.getElementById(id).contentWindow.document.body.scrollHeight;
+	}
+	document.getElementById(id).style.height= (newheight + 16) + "px";
+}
